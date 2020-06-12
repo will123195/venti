19979dde823019ad2172d2395039eaf59bd3b56b
will123195 <will123195@gmail.com>
2020-06-12 14:33:11 -0600

    npm update and fixed test warnings

--- tests/useVenti.spec.js
+++ tests/useVenti.spec.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { useVenti, state } from '..'
 import { mount, configure } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
+import { act } from 'react-dom/test-utils'
 
 configure({ adapter: new Adapter() })
 
@@ -33,32 +34,32 @@ describe('useVenti', function () {
   })
 
   it('should update book', function () {
-    state.set('books.a', { id, title, price })
+    act(() => state.set('books.a', { id, title, price }))
     const wrapper = mount(<Book id={id} />)
-    state.set('books.a', { id, title, price: 29.99 })
+    act(() => state.set('books.a', { id, title, price: 29.99 }))
     expect(wrapper.find('.price').text()).toBe('29.99')
     wrapper.unmount()
   })
 
   it('should update price', function () {
-    state.set('books.a', { id, title, price })
+    act(() => state.set('books.a', { id, title, price }))
     const wrapper = mount(<Book id={id} />)
-    state.set('books.a.price', 39.99)
+    act(() => state.set('books.a.price', 39.99))
     expect(wrapper.find('.price').text()).toBe('39.99')
     wrapper.unmount()
   })
 
   it('should remove listeners when unmounted', function () {
-    state.set('books.a', { id, title, price })
+    act(() => state.set('books.a', { id, title, price }))
     const wrapper = mount(<Book id={id} />)
-    state.set('books.a.price', 1.99)
+    act(() => state.set('books.a.price', 1.99))
     expect(wrapper.find('.price').text()).toBe('1.99')
     expect(Object.keys(state.listeners).length).toBe(1)
     wrapper.unmount()
     expect(Object.keys(state.listeners).length).toBe(0)
-    state.set('books.a.price', 2.99)
+    act(() => state.set('books.a.price', 2.99))
     const wrapper2 = mount(<Book id={id} />)
-    state.set('books.a.price', 3.99)
+    act(() => state.set('books.a.price', 3.99))
     expect(wrapper2.find('.price').text()).toBe('3.99')
     wrapper2.unmount()
   })
